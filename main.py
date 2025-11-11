9f3a6aa8f63e6e275a8be2d04869dad331403000
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-11-11 17:08:38 +0200

    save and load file

--- main.py
+++ main.py
@@ -30,20 +30,52 @@ def show_list(shopping_list):
         # print(i, item)
         print(f"{i}. {item["name"]} - {item["quantity"]} x {item["price"]}€")
 
-def count_total():
-    pass
+def count_total(shopping_list):
+    total = 0
+    for item in shopping_list:
+        total += item["quantity"] * item["price"]
+    print(f"Total price: {total:.2f}€")
 
-def save_to_file():
-    pass
+def save_to_file(shopping_list):
+    # "w" - перезаписує файл якщо той є або створює новий, якщо немає
+    # "a" - дописує (за замовчуванням у кінці файлу) у існуючий файл, якщо файлу немає - помилка
+    # file = open("text.txt", "w", encoding="utf-8")
+    # file.write("Ok")
+    # file.write("2 line")
+    # file.write("new_line")
+    # file.close()
+    # with open("text.txt", "w", encoding="utf-8") as f:
+    #     f.write("Ok")
+    #     f.write("2 line")
+    #     f.write("new_line")
+    with open("text.txt", "w", encoding="utf-8") as f:
+        for i, item in enumerate(shopping_list, start=1):
+            # print(i, item)
+            f.write(f"{i}. {item["name"]} - {item["quantity"]} x {item["price"]}€\n")
+    print("✅Shopping_list saved to text.txt")
 
 def load_from_file():
-    pass
+    shopping_list = []
+    with open("text.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            line_list = line.strip()[:-1].split()
+            # for i in range(1, len(line_list), 2):
+            name, quantity, price = line_list[1], line_list[3], line_list[5]
+            item = {
+                "name": name,
+                "quantity": int(quantity),
+                "price": float(price)
+            }
+            shopping_list.append(item)
+    return shopping_list
+        
 
 def main():
     print("🛒 Вітаю у менеджері покупок!")
     shopping_list = []
     
     while True:
+        print("------------------------------")
         print('''
 Меню:
 1. Додати покупку
@@ -69,9 +101,9 @@ def main():
                 case 2:
                     show_list(shopping_list)
                 case 3:
-                    count_total()
+                    count_total(shopping_list)
                 case 4:
-                    save_to_file()
+                    save_to_file(shopping_list)
                 case 5:
                     load_from_file()
                 case 6:
@@ -82,4 +114,34 @@ def main():
                     
         except ValueError:
             print("Enter number 1-6!!")
-main()
+
+
+if __name__ == "__main__":
+    main()
+    
+# try:  # перша основна перевірка
+#     "program"
+#     result = "OK"
+# except ValueError:
+#     print("ValueError")
+#     result = "ValueError"
+# except ZeroDivisionError:
+#     print()
+#     result = "ZeroDivisionError"
+# except:
+#     print("any errors")
+#     result = "any errors"
+# else:
+#     result = "else"
+#     pass
+# finally:  # виконується завжди!
+#     print(result)
+#     pass
+
+# try -> finally
+# try -> except -> ... -> except -> finally
+# try -> except -> ... -> except --> else -> finally
+
+# try -> finally
+# except -> finally
+# else -> finally
